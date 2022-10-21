705895e51758c2c525bb972f3b346b3abbe94a6a
AlanFarias97 <alanfarias_97@hotmail.com>
2022-10-21 00:54:26 -0300

    initial

---
+++ js/index.js
@@ -0,0 +1,46 @@
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+/* function validate() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (username == "Formget" && password == "formget#123") {
+        alert("Login successfully");
+        window.location = "success.html"; // Redirecting to other page.
+        return false;
+    }
+    else {
+        attempt--;// Decrementing by one.
+        alert("You have left " + attempt + " attempt;");
+        // Disabling fields after 3 attempts.
+        if (attempt == 0) {
+            document.getElementById("username").disabled = true;
+            document.getElementById("password").disabled = true;
+            document.getElementById("submit").disabled = true;
+            return false;
+        }
+    }
+} */
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "web_dev") {
+        alert("You have successfully logged in.");
+        location.reload();
+    } else {
+        attempt--;// Decrementing by one.
+        alert("You have left " + attempt + " attempt;");
+        // Disabling fields after 3 attempts.
+        if (attempt == 0) {
+            loginForm.username.setAttribute("disabled","disabled")
+            loginForm.password.setAttribute("disabled","disabled")
+            document.getElementById("login-form-submit").setAttribute("disabled","disabled")
+        }
+    }
+})
